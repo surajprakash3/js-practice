90586579859916fd21891b8dd960666260c084d7
Suraj Prakash <surajprak201@gmail.com>
2025-12-08 04:53:03 +0530

    first commit

--- 5_string.js
+++ 5_string.js
@@ -7,8 +7,8 @@ console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);
 
 const gameName = new String('hitesh-hc-com')
 
-// console.log(gameName[0]);
-// console.log(gameName.__proto__);
+console.log(gameName[0]);
+console.log(gameName.__proto__);
 
 
 // console.log(gameName.length);
