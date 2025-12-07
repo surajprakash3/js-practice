79ac2eede7407a74df3743c2153ace8d9e78ffd2
Suraj Prakash <surajprak201@gmail.com>
2025-12-08 04:33:51 +0530

    commit

---
+++ 2_datatype.js
@@ -0,0 +1,43 @@
+"use strict"; //treat all js code as newer version
+
+//alert(3+3)// we are using nodejs, not browser
+
+console.log(3 + 3 ) 
+console.log("suraj")
+
+
+let name = "suraj"
+let age = 18
+let isLoggedIn = false
+let state= null
+
+//number => 2 to power 53
+// bigint 
+//string = ""
+//boolean => true/false
+//null =>standalone value 
+// undefined =>
+//symbol -> unique
+
+//object 
+
+console.log(typeof "suraj");
+console.log(typeof null);
+console.log(typeof undefined);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ 2_variable.js
@@ -0,0 +1,17 @@
+const accountId =144553
+let accountEmail="sura@google.com"
+var accountPassword ="12345"
+accountCity="Jaipur"
+let accountState;
+
+// accountId = 2 not allowed
+
+accountEmail ="hsd@d.com"
+accountPassword = "21212121"
+accountCity = "Bengaluru"
+console.log(accountId);
+
+// prefrer not to use var
+// because of issue in block scope and function scope 
+
+console.table([accountId, accountEmail, accountPassword, accountCity])
