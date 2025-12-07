bc2945db882813722745fb170a4905c9eb4159ad
Suraj Prakash <surajprak201@gmail.com>
2025-12-08 04:31:19 +0530

    basic commit

--- 5_string.js
+++ 5_string.js
@@ -13,7 +13,7 @@ const gameName = new String('hitesh-hc-com')
 
 // console.log(gameName.length);
 // console.log(gameName.toUpperCase());
-console.log(gameName.charAt(2));
+// console.log(gameName.charAt(2));
 console.log(gameName.indexOf('t'));
 
 const newString = gameName.substring(0, 4)
